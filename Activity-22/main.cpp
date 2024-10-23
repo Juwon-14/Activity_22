68ff48a91f9feacb2d0f46df8cded854271868e1
Juwon-14 <wndnjs9127@gmail.com>
2024-10-23 11:42:10 -0700

    Update main.cpp

--- Activity-22/main.cpp
+++ Activity-22/main.cpp
@@ -2,13 +2,29 @@
 //  main.cpp
 //  Activity-22
 //
-//  Created by 홍주원 on 10/19/24.
+//  Created by Juwon Hong on 10/19/24.
 //
 
 #include <iostream>
+#include <string>
+#include "function.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    // Set the full, first, and last name
+    string fullName = "Juwon Hong";
+    string firstName, lastName;
+
+    // Split the first and last name
+    splitName(fullName, firstName, lastName);
+
+    // Print first name and last name
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+
+    // Greeting full name
+    greet(fullName);
+
     return 0;
 }
