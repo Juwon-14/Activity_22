406b75ec3670aee79b4cf4d49a0d3c85976b5a8f
Juwon-14 <wndnjs9127@gmail.com>
2024-10-23 11:43:16 -0700

    Add files via upload

---
+++ Activity-22/function.h
@@ -0,0 +1,20 @@
+//
+//  function.h
+//  Activity-22
+//
+//  Created by Juwon on 10/23/24.
+//
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <string>
+using namespace std;
+
+// Function to print a greeting
+void greet(const string& name);
+
+// Function to split the full name into first name and last name
+void splitName(const string& fullName, string& firstName, string& lastName);
+
+#endif
