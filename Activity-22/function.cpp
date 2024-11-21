42b0fe39640e336324f2e0d44f03b132c42654f1
Juwon-14 <wndnjs9127@gmail.com>
2024-11-21 10:58:17 -0800

    Update function.cpp

--- Activity-22/function.cpp
+++ Activity-22/function.cpp
@@ -9,15 +9,13 @@
 #include <string>
 #include "function.h"
 
-using namespace std;
-
 // Function to print a greeting
-void greet(const string& name) {
-    cout << "Hello, " << name << endl;
+void greet(const std::string& name) {
+    std::cout << "Hello, " << name << std::endl;
 }
 
 // Function to split the full name into first name and last name
-void splitName(const string& fullName, string& firstName, string& lastName) {
+void splitName(const std::string& fullName, std::string& firstName, std::string& lastName) {
     size_t spaceIndex = fullName.find(' ');
     firstName = fullName.substr(0, spaceIndex);
     lastName = fullName.substr(spaceIndex + 1);
